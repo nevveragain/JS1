5480ce5906aba8592c64b1a80704788600c2b8bc
nevveragain <72740621+nevveragain@users.noreply.github.com>
2020-11-10 12:13:12 +0500

    final

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var setName = prompt('Введите своё имя')
+console.log('Меня звать ' + setName);
+
+var age = +prompt('Сколько вам лет?')
+console.log('Мне ' + age);
+
+var ex = prompt ('Решите пример: 2 + 2');
+console.log('2 + 2 = 4 Ваш ответ = ' + ex);
+var ex2 = prompt ('Решите пример: 6 - 2');
+console.log('6 - 4 = 2 Ваш ответ = ' + ex2);
+var ex3 = prompt ('Решите пример: 4 * 2');
+console.log('4 * 2 = 8 Ваш ответ = ' + ex3);
+var ex4 = prompt ('Решите пример: 12 / 3');
+console.log('12 / 3 = 4 Ваш ответ = ' + ex4);
+var ex5 = prompt ('Решите пример = 20 % 3');
+console.log ('20 % 3 = 2 Ваш ответ = ' + ex5);
+
+alert('Чтобы узнать ответы войдите в консоль')
